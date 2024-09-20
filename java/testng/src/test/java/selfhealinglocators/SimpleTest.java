db1e438e83a41263cabac44fe5a47e366a404b29
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-09-20 10:40:00 -0600

    Adding configuration file of healenium and updating gitignore file.

--- java/testng/src/test/java/selfhealinglocators/SimpleTest.java
+++ java/testng/src/test/java/selfhealinglocators/SimpleTest.java
@@ -1,12 +1,18 @@
 package selfhealinglocators;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import com.epam.healenium.SelfHealingDriverWait;
 
 import selfhealinglocators.DriverManager.BrowserType;
 
@@ -40,7 +46,12 @@ public class SimpleTest {
         String expectedMessage = "Hello";
         _driver.navigate().to(url);
         _driver.findElement(By.xpath("//input[@id='messageNew']")).sendKeys(expectedMessage);
-        _driver.findElement(By.xpath(updateButtonLocator)).click();
+        
+        // beginning of critical section
+        new SelfHealingDriverWait(_driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(updateButtonLocator))).click();
+        // end of critical section
+
         String current = _driver.findElement(By.xpath("//p[1]")).getText();
         Assert.assertTrue(current.contains(expectedMessage));
     }
