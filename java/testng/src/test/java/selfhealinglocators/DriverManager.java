0b3df3b2f8f0272dd5adec851238e3df06190eb4
Juan Fonseca <juan.fonseca@gorillalogic.com>
2024-09-30 16:40:11 -0600

    Fixing yaml...

--- java/testng/src/test/java/selfhealinglocators/DriverManager.java
+++ java/testng/src/test/java/selfhealinglocators/DriverManager.java
@@ -12,7 +12,8 @@ public class DriverManager
     public static WebDriver GetWebDriver(DriverType driverType) throws Exception
     {
         @SuppressWarnings("deprecation")
-        URL _hubUrl = new URL("http://127.0.0.1:8085/wd/hub");
+        //URL _hubUrl = new URL("http://127.0.0.1:8085/wd/hub");
+        URL _hubUrl = new URL("http://hlm-proxy:8085/wd/hub");
 
         switch (driverType) {
 
